2e5f0a1aa3321b112cb764bd522307a6932ae739
luoxiaoxiao <3060928836@qq.com>
2018-09-21 18:29:11 +0800

    注释修改

--- src/integration-test/java/com/xiao/gs/controller/GreetingControllerTest.java
+++ src/integration-test/java/com/xiao/gs/controller/GreetingControllerTest.java
@@ -8,10 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,7 +30,7 @@ public class GreetingControllerTest {
     @Test
     public void greetingShouldReturnMsgFromService() throws Exception {
         when(greetingService.greet()).thenReturn("Hello Mock");
-        mockMvc.perform(MockMvcRequestBuilders.get("/greeting"))
+        mockMvc.perform(get("/greeting"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello Mock")));
