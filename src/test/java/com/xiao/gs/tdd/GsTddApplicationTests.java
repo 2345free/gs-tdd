517e0d1d015d9df7fc09ea4d49b93e5742670f15
luoxiaoxiao <3060928836@qq.com>
2018-09-21 15:35:40 +0800

    mock测试

--- src/test/java/com/xiao/gs/tdd/GsTddApplicationTests.java
+++ src/test/java/com/xiao/gs/tdd/GsTddApplicationTests.java
@@ -2,15 +2,31 @@ package com.xiao.gs.tdd;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+// @AutoConfigureMockMvc 启动完整的上下文但是不启动服务器
+@AutoConfigureMockMvc
 public class GsTddApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    public void contextLoads() {
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
     }
 
 }
---
+++ src/test/java/com/xiao/gs/tdd/HttpRequestTest.java
@@ -0,0 +1,26 @@
+package com.xiao.gs.tdd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+// 使用 webEnvironment=RANDOM_PORT 随机端口启动服务器（有助于避免测试环境中的冲突）
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() {
+        assertThat(restTemplate.getForObject("/", String.class))
+                .contains("Hello World");
+    }
+}
