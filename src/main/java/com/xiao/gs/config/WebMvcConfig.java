89e0fc9af071c662d033b95e9f9af06b88981e42
luoxiaoxiao <2345free@sina.com>
2018-10-02 18:59:17 +0800

    WebMvcConfigurationSupport,@EnableWebMvc //NOTE: Only needed in a non-springboot application

--- src/main/java/com/xiao/gs/config/WebMvcConfig.java
+++ src/main/java/com/xiao/gs/config/WebMvcConfig.java
@@ -2,15 +2,15 @@ package com.xiao.gs.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
+ * mvc配置
+ *
  * @author luoxiaoxiao
  */
-@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.xiao.gs.controller")
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 }
