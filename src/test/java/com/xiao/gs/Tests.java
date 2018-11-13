9ee0663e2ec464e5df0f471d6cce07c6212fe5da
luoxiaoxiao <2345free@sina.com>
2018-11-13 12:47:17 +0800

    算数运算优先级测试

--- src/test/java/com/xiao/gs/Tests.java
+++ src/test/java/com/xiao/gs/Tests.java
@@ -16,4 +16,23 @@ public class Tests {
         System.out.println(now2);
     }
 
+    @Test
+    public void test2() {
+        int i = 1;
+        int j = i++;
+        if ((i == (++j)) && ((i++) == j)) {
+            i += j;
+        }
+        System.out.println(i); // 5
+    }
+
+    @Test
+    public void test3() {
+        int num = 50;
+        num = num++ * 2;
+        System.out.println(num); // 100
+    }
+
+
 }
+
