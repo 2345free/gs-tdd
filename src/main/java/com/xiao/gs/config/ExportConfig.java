4ec2795f724d33c2945d15b7edc81c8c9c8552e8
luoxiaoxiao <xiaoxiao.luo@tendata.cn>
2018-11-29 11:26:03 +0800

    启用请求路径后缀匹配模式

--- src/main/java/com/xiao/gs/config/ExportConfig.java
+++ src/main/java/com/xiao/gs/config/ExportConfig.java
@@ -10,6 +10,7 @@ import org.springframework.oxm.xstream.XStreamMarshaller;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.BeanNameViewResolver;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
@@ -37,6 +38,14 @@ public class ExportConfig implements WebMvcConfigurer {
         resolver.setViewResolvers(resolvers);
         resolver.setContentNegotiationManager(manager);
         return resolver;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // 请求路径后缀匹配模式
+        configurer.setUseSuffixPatternMatch(true);
+        // 请求路径最后面是否有斜杠都是同一个接口
+        configurer.setUseTrailingSlashMatch(true);
     }
 
     @Override
